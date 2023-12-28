e13f5cb5a04c7204345963cd5749b7d7150a25f2
fnaufalh <fnaufalh@gmail.com>
2023-12-29 06:40:42 +0700

    add .cpanel.yml

--- config/plugins.js
+++ config/plugins.js
@@ -11,5 +11,21 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  // "netlify-deployments": {
+  //   enabled: true,
+  //   config: {
+  //     accessToken: env.NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN,
+  //     // accessToken: "nfp_Rtv4XCC8WPHa3MjLZDTVmcXBikVvWwyt6c17",
+  //     sites: [
+  //       {
+  //         name: "Pinst CMS",
+  //         id: "0cba3a29-1b47-4b9c-b3ba-2f73f54c3670",
+  //         buildHook:
+  //           "https://api.netlify.com/build_hooks/658df7e9541fd53d22004e94",
+  //         branch: "main", // optional
+  //       },
+  //     ],
+  //   },
+  // },
   // ...
 });
